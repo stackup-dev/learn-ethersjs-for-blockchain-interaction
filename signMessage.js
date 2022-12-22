f4c2f91afc22561705ba1b7c22ea8cbfb0ce14be
stackup-dev <hello@stackup.dev>
2022-12-22 11:26:22 +0800

    first commit

---
+++ signMessage.js
@@ -0,0 +1,15 @@
+const { ethers } = require("ethers");
+
+const signMessage = async () => {
+  // Create a provider instance
+  const provider = new ethers.providers.JsonRpcProvider("https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161");
+
+  // Create a wallet instance
+  const wallet = new ethers.Wallet("0x999cdfc447ae64af13413525f02cfc82a13d1ee40ecc3acf9978d290f8b10515", provider);
+
+  // Sign the message
+  const output = await wallet.signMessage("ethers.js is a powerful library"); // INSERT METHOD HERE
+  console.log("signature of message:", output);
+};
+
+signMessage();
